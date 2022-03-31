71e9fea151a9755d6b2e35a8bf71b976d5c0655d
Oladejo Azeez <az4arewa.abb@gmail.com>
2022-03-31 22:12:25 +0100

    add unit test

---
+++ TestingPackage/ValidSubsequenceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace TestingPackage
+{
+    [TestClass]
+    public class ValidSubsequenceTest
+    {
+        [TestMethod]
+        public void IsValidSequence()
+        {
+            List<int> array = new()
+            {
+                5, 1, 22, 25, 6, -1, 8, 10
+            };
+
+            List<int> sequence = new()
+            {
+                1, 6, -1, 10
+            };
+
+            Assert.IsTrue(ValidateSequence.Program.IsValidSubsequence(array, sequence));
+        }
+    }
+}
---
+++ ValidateSequence/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValidateSequence
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static bool IsValidSubsequence(List<int> array, List<int> sequence)
+        {
+            int arrayIndex = 0;
+            int sequenceIndex = 0;
+
+            while (arrayIndex < array.Count && sequenceIndex < sequence.Count)
+            {
+                if (array[arrayIndex] == sequence[sequenceIndex])
+                {
+                    sequenceIndex++;
+                }
+                arrayIndex++;
+            }
+
+            return sequenceIndex == sequence.Count;
+        }
+    }
+
+}
